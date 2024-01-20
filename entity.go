66d149f2918eeb9483f59ea23fde1c552546ed55
Maikel Veen <veenmaikel@gmail.com>
2024-01-20 19:36:22 +0100

    Add Entity type

---
+++ entity.go
@@ -0,0 +1,7 @@
+package main
+
+// MaxEntities is the maximum number of entities that can be created.
+const MaxEntities = 256
+
+// Entity is a unique identifier for a game object.
+type Entity uint8
